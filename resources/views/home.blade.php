bb0321a5c8971fca40babe041513f50c728667a4
mostdream <mostdream@naver.com>
2025-12-11 17:36:02 +0900

    1.스타일 깨짐 수정.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -197,7 +197,7 @@
                 </a>
             </div>
         </div>
-        <div id="carouselExampleCaptions" data-bs-ride="carousel" data-bs-interval="2400" class="carousel slide mb-5">
+        <div id="carouselExampleCaptions" data-bs-ride="carousel" data-bs-interval="2400" class="carousel slide" style="margin-bottom:85px;">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
